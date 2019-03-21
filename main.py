b1be590e175fd327159f3a85aa10c7c38019ccd0
Donaim <3Donaim@gmail.com>
2019-03-21 14:01:20 +0100

    branching out

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 
 import subprocess
+import sys
 
+DEVEL_ENDING = '-devel'
 
 def ex(cmd: str) -> str:
 	print('ex: {}'.format(cmd))
@@ -14,8 +16,33 @@ def ex(cmd: str) -> str:
 def exgit(s: str) -> str:
 	return ex('git ' + s)
 
-def gitlog(s: str) -> list:
-	raw = exgit('git log ')
+def get_parsed_log() -> list:
+	raw = exgit("log --format='%H %t'").strip()
+	lines = raw.split('\n')
+	ret = []
+	for line in lines:
+		ret.append(line.split())
+	return ret
+
+def get_current_branch() -> str:
+	return exgit('rev-parse --abbrev-ref HEAD').strip()
 
 
+def gassert(b: bool, message: str) -> None:
+	if not b:
+		print('{}'.format(message), file=sys.stderr)
+		exit(1)
+
+print('hello')
+
+# print('curent branch: ' + get_current_branch().strip())
+# print(get_parsed_log())
+
+current_branch = get_current_branch()
+gassert(current_branch.endswith(DEVEL_ENDING), 'should end with -devel')
+
+main_branch = current_branch[:-len(DEVEL_ENDING)]
+gassert(len(main_branch) > 0, 'invalid current branch name')
+
+print('main branch = "{}"'.format(main_branch))
 
