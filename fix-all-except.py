994a4d3dceee4783ff39d0e56919ac1fb3a45efc
Donaim <3Donaim@gmail.com>
2019-03-21 15:39:37 +0100

    args parsing

--- fix-all-except.py
+++ fix-all-except.py
@@ -1,5 +1,12 @@
 #! /usr/bin/env python3
 
+import sys
 
-print ('hello!')
+
+args = sys.argv[1:]
+
+file    = args[-1]
+exclude = args[:-1]
+
+print('file = "{}" exclude = {}'.format(file, exclude))
 
