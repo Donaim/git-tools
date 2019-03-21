1297e1b88e65573fe34ca067a76b345f6236a774
Donaim <3Donaim@gmail.com>
2019-03-21 15:01:25 +0100

    common ancestor

--- main.py
+++ main.py
@@ -27,6 +27,18 @@ def get_parsed_log() -> list:
 def get_current_branch() -> str:
 	return exgit('rev-parse --abbrev-ref HEAD').strip()
 
+def branch_exists_q(name: str) -> bool:
+	try:
+		branches = exgit('branch')
+		return any([b.strip() == name for b in branches.split('\n')])
+	except:
+		return False
+
+def get_common_ancestor(branch1: str, branch2: str) -> str:
+	return exgit('merge-base "{}" "{}"'.format(branch1, branch2))
+
+def get_branch_hash(name: str) -> str:
+	return exgit('rev-parse "{}"'.format(name))
 
 def gassert(b: bool, message: str) -> None:
 	if not b:
@@ -44,5 +56,17 @@ gassert(current_branch.endswith(DEVEL_ENDING), 'should end with -devel')
 main_branch = current_branch[:-len(DEVEL_ENDING)]
 gassert(len(main_branch) > 0, 'invalid current branch name')
 
-print('main branch = "{}"'.format(main_branch))
+# print('main branch = "{}"'.format(main_branch))
+# print('exists: ' + str(branch_exists_q('master')))
+
+common_ancestor = None
+
+if branch_exists_q(main_branch):
+	print('"{}" exists'.format(main_branch))
+	common_ancestor = get_common_ancestor(main_branch, current_branch)
+else:
+	print('"{}" does not exist'.format(main_branch))
+	common_ancestor = None
+
+print('common ancestor: {}'.format(common_ancestor))
 
