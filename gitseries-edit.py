7452b2274d872235d93233ad22b08eaf169c4c3a
Donaim <3Donaim@gmail.com>
2019-03-26 00:35:35 +0000

    new command: gitseries-edit

---
+++ gitseries-edit.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+from common import *
+import gitseries
+
+cs = gitseries.CurrentSeries()
+
+commits = list(reversed(cs.empty_commits))
+
+print('')
+print('Series:')
+for (i, c) in enumerate(commits):
+	print('\t{}) {}'.format(i, c.s))
+
+index  = int(input('Chosen: '))
+if index == 0:
+	exit(0)
+
+# Get last commit of that patch series
+next   = commits[index - 1].H
+edit   = get_commit_hash('{}~1'.format(next))
+
+run_editor(['--edit', edit], edit)
