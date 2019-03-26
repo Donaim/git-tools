a5f4ec9358d98770056017c0768019c0242cb5be
Donaim <3Donaim@gmail.com>
2019-03-26 00:35:34 +0000

    simplify if main branch does not exist

--- gitseries.py
+++ gitseries.py
@@ -15,12 +15,18 @@ class CurrentSeries:
 
 		if branch_exists_q(self.main_branch):
 			print('"{}" exists'.format(self.main_branch))
+			self.common_ancestor = get_common_ancestor(self.main_branch, self.current_branch)
 		else:
 			print('"{}" does not exist'.format(self.main_branch))
-			if create: self.create_main_branch()
-			else: return
+			last = None
+			last_tag = SERIES_BEGIN_TAG_PREFIX + self.main_branch
+			if check_tag_exists(last_tag):
+				last = last_tag
+				print('using {} as rebase point'.format(last_tag))
+			else:
+				print('tag {} does not exists -> using --root as rebase point'.format(last_tag))
+			self.common_ancestor = last
 
-		self.common_ancestor = get_common_ancestor(self.main_branch, self.current_branch)
 		print('common ancestor: {}'.format(get_commit_print(self.common_ancestor)))
 
 		self.commits = get_commits(self.common_ancestor)
@@ -30,29 +36,11 @@ class CurrentSeries:
 		self.rebase_point = self.rebase_commit.H
 		print('rebase point:\n\t{}'.format(str(self.rebase_commit)))
 
-	def create_main_branch(self):
-		last = None
-		last_tag = SERIES_BEGIN_TAG_PREFIX + self.main_branch
-		if check_tag_exists(last_tag):
-			last = last_tag
-			print('using {} as rebase point'.format(last_tag))
-		else:
-			print('tag {} does not exists -> using --root as rebase point'.format(last_tag))
-
-		all_commits = get_commits(last)
-		empty_ones = get_empty_commits(all_commits)
-		print('empty commits: \n\t{}'.format('\n\t'.join(str(c) for c in empty_ones)))
-
-		first_empty = empty_ones[0]
-		exout('git checkout "{}"~1 --quiet'.format(first_empty.H))
-		exoutn('git branch "{}"'.format(self.main_branch))
-		exoutn('git checkout "{}" --quiet'.format(self.current_branch))
-		print("Created main branch")
-
 if __name__ == '__main__':
 	cs = CurrentSeries(True)
 
-	exout('git branch -D "{}"'.format(cs.main_branch))
+	try: exout('git branch -D "{}"'.format(cs.main_branch))
+	except: pass
 	exout('git checkout -b "{}"'.format(cs.main_branch))
 
 	not_empty = [c for c in cs.commits if c not in cs.empty_commits]
